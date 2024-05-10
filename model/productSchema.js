aafa11121cfe372d2d887a4835271ab551302dbb
dheeraj <dheerajrvithalkar@gmail.com>
2024-05-10 14:37:46 +0530

    restructuring and cleaning up

--- model/productSchema.js
+++ model/productSchema.js
@@ -1,30 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Please Enter product name"],
-        },
-        price: {
-            type: Number,
-            required: true,
-            default:0
-        },
-        image: {
-            type: String,
-            required: false,
-        },
-        quantity: {
-            type: Number,
-            required: true,
-            default:0
-        }
+  {
+    name: {
+      type: String,
+      required: [true, "Please Enter product name"],
     },
-    {
-        timestamps: true,
-    }
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model("Product", ProductSchema
-)
+module.exports = mongoose.model("Product", ProductSchema);
